f9b015267b3e6d7a853fcf38744c7a861215cc69
Isaac Ruiz Guerra <rugi.eolos@gmail.com>
2011-11-25 19:00:58 -0600

    clase principal

--- client/src/main/java/org/xhubacubi/jarhalla/App.java
+++ client/src/main/java/org/xhubacubi/jarhalla/App.java
@@ -1,13 +1,19 @@
 package org.xhubacubi.jarhalla;
 
+import javax.swing.WindowConstants;
+import org.xhubacubi.jarhalla.client.ui.JMain;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "jarhalla Client" );
+public class App {
+
+    public static void main(String[] args) {
+        JMain main = new JMain();
+        main.setSize(300, 300);
+        main.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        main.pack();
+        main.setVisible(true);
     }
 }
---
+++ client/src/main/java/org/xhubacubi/jarhalla/client/ui/JMain.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates and open the template in
+ * the editor.
+ */
+package org.xhubacubi.jarhalla.client.ui;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author rugi
+ */
+public class JMain extends JFrame {
+ public JMain(){
+     super();
+ }   
+ 
+}
