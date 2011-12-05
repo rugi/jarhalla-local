f1d60a6ba9d12bcb0a77ff896dfc064a2e06cffb
Isaac Ruiz Guerra <rugi.eolos@gmail.com>
2011-12-04 20:38:35 -0600

    Menu y status bar

--- client/src/main/java/org/xhubacubi/jarhalla/client/ui/JMain.java
+++ client/src/main/java/org/xhubacubi/jarhalla/client/ui/JMain.java
@@ -4,7 +4,15 @@
  */
 package org.xhubacubi.jarhalla.client.ui;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import org.xhubacubi.jarhalla.client.ui.components.StatusBar;
 
 /**
  *
@@ -12,14 +20,44 @@ import javax.swing.JFrame;
  */
 public class JMain extends JFrame {
 
-    
+    private JMenuBar menuBar;
+    private JMenu mMain;
+    private JMenu mExit;
+    private JMenuItem mIExit;
+    private StatusBar status;
+
     public JMain() {
         super();
         initComponents();
     }
-    
-    public void initComponents(){
-    
+
+    public void initComponents() {
+        //primero el menu
+        mIExit = new JMenuItem("Exit",
+                KeyEvent.VK_E);
+
+        mExit = new JMenu("Exit");
+        mExit.setMnemonic(KeyEvent.VK_X);
+        mExit.add(mIExit);
+        mIExit.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                System.exit(0);
+            }
+        });
+
+        mMain = new JMenu("Config");
+        mMain.setMnemonic(KeyEvent.VK_R);
+        menuBar = new JMenuBar();
+        menuBar.add(mMain);
+        menuBar.add(mExit);
+        this.setJMenuBar(menuBar);
+        //luego el statusbar
+        status = new StatusBar();
+
+        //Y ahora el layout.
+        this.setLayout(new BorderLayout());
+        this.add(status, BorderLayout.SOUTH);
     }
-    
 }
---
+++ client/src/main/java/org/xhubacubi/jarhalla/client/ui/components/StatusBar.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates and open the template in
+ * the editor.
+ */
+package org.xhubacubi.jarhalla.client.ui.components;
+
+import java.awt.Dimension;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author rugi
+ */
+public class StatusBar extends JLabel {
+    
+    /** Creates a new instance of StatusBar */
+    public StatusBar() {
+        super();
+        super.setPreferredSize(new Dimension(100, 16));
+        setMessage("Ready");
+    }
+    
+    public void setMessage(String message) {
+        setText(" "+message);        
+    }        
+}
