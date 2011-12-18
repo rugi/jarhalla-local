f9338816d3fc84446556d26f2cf723feda4b8682
Isaac Ruiz Guerra <rugi.eolos@gmail.com>
2011-12-18 01:33:04 -0600

    agregado evento de seleccion de directio

--- client/src/main/java/org/xhubacubi/jarhalla/App.java
+++ client/src/main/java/org/xhubacubi/jarhalla/App.java
@@ -12,20 +12,20 @@ import org.xhubacubi.jarhalla.client.ui.JPanelRepository;
 public class App {
 
     public static void main(String[] args) {
-        JMain main = new JMain();        
-        main.setSize(300, 300);
-        main.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        main.pack();
-        main.setVisible(true);
-        
-        
-        // Para probar el componente de listado de repositorios.
-//        JPanelRepository repos = new JPanelRepository();
-//        JFrame main = new JFrame();
-//        main.add(repos);
+//        JMain main = new JMain();        
 //        main.setSize(300, 300);
 //        main.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 //        main.pack();
-//        main.setVisible(true);        
+//        main.setVisible(true);
+        
+        
+        // Para probar el componente de listado de repositorios.
+        JPanelRepository repos = new JPanelRepository();
+        JFrame main = new JFrame();
+        main.add(repos);
+        main.setSize(300, 300);
+        main.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        main.pack();
+        main.setVisible(true);        
     }
 }
--- client/src/main/java/org/xhubacubi/jarhalla/client/ui/JPanelRepository.java
+++ client/src/main/java/org/xhubacubi/jarhalla/client/ui/JPanelRepository.java
@@ -5,6 +5,10 @@
 package org.xhubacubi.jarhalla.client.ui;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.EtchedBorder;
@@ -20,9 +24,19 @@ public class JPanelRepository extends JPanel {
     private JList listRepos;
     private DefaultListModel reposModel;
     private JSplitPane split;
-
     private Border loweredetched = BorderFactory.createEtchedBorder(EtchedBorder.LOWERED);
-    private TitledBorder  title;
+    private TitledBorder title;
+    private TitledBorder titleDir;
+    private TitledBorder titleLog;
+    private JScrollPane scrollLog;
+    private JTextArea log;
+    private JProgressBar progress;
+    private JPanel panelDetail;
+    private JPanel panelSelect;
+    private JFileChooser chooser;
+    private JTextField directory;
+    private JButton buttonSearch;
+
     public JPanelRepository() {
         super();
         initComponents();
@@ -74,13 +88,62 @@ public class JPanelRepository extends JPanel {
         listRepos.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         scrollRepos = new JScrollPane(listRepos);
 
+        // panel seleccion
+        chooser = new JFileChooser();
+        directory = new JTextField();
+        //chooser.setCurrentDirectory(new java.io.File("."));
+        chooser.setDialogTitle("Selecciona el directorio a indexar");
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);        
+        directory.addFocusListener(new FocusListener() {
 
-        split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, scrollRepos, null);
+            @Override
+            public void focusGained(FocusEvent fe) {
+                if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                    directory.setText(chooser.getCurrentDirectory().getAbsolutePath());
+                    buttonSearch.requestFocusInWindow();
+                    
+                } else {
+                    System.out.println("No Selection ");
+                }
+            }
+
+            @Override
+            public void focusLost(FocusEvent fe) {
+                //throw new UnsupportedOperationException("Not supported yet.");
+            }
+        });
+
+
+        titleDir = BorderFactory.createTitledBorder(
+                loweredetched, "Directorio:");
+        titleLog = BorderFactory.createTitledBorder(
+                loweredetched, "Salida:");        
+        panelSelect = new JPanel();
+        panelSelect.setLayout(new BorderLayout());
+        panelSelect.add(directory, BorderLayout.CENTER);
+                              
+        
+        buttonSearch= new JButton("Buscar");
+        panelSelect.add(buttonSearch, BorderLayout.SOUTH);
+        panelSelect.setBorder(titleDir);
+        // panel Details
+        log = new JTextArea();
+        log.setEditable(false);
+
+        scrollLog = new JScrollPane(log);
+        scrollLog.setBorder(titleLog);
+        progress = new JProgressBar();
+        panelDetail = new JPanel();
+        panelDetail.setLayout(new BorderLayout());
+        panelDetail.add(panelSelect, BorderLayout.NORTH);
+        panelDetail.add(scrollLog, BorderLayout.CENTER);
+        panelDetail.add(progress, BorderLayout.SOUTH);
+        split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, scrollRepos, panelDetail);
         split.setOneTouchExpandable(true);
         split.setDividerLocation(150);
         this.add(split, BorderLayout.CENTER);
-        title=BorderFactory.createTitledBorder(
-                       loweredetched, "Repository List:");
+        title = BorderFactory.createTitledBorder(
+                loweredetched, "Repository List:");
         title.setTitleJustification(TitledBorder.LEFT);
         this.setBorder(title);
     }
